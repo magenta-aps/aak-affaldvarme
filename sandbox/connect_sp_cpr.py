5dd0953fe35eec36c90643a15763878e0ee556c2
Ubbe Welling <ubbe@magenta.dk>
2017-04-10 14:00:35 +0200

    Setup of CPR subscription on prod environment now working, as well as SFTP fetch of files

--- sandbox/connect_sp_cpr.py
+++ sandbox/connect_sp_cpr.py
@@ -4,13 +4,13 @@ from requests import Session
 from zeep.transports import Transport
 
 
-def init_service(endpoint_url, service_agreement_uuid, user_system_uuid, user_uuid, service_uuid):
+def init_service(endpoint_url, service_agreement_uuid, user_system_uuid, user_uuid, service_uuid, cert_filename):
     handler = logging.StreamHandler()
     logging.getLogger("zeep.wsdl.bindings.soap").addHandler(handler)
     wsdl_url = 'CprSubsc/CprSubscriptionService.wsdl'
 
     session = Session()
-    session.cert = './magenta_ava_test_2017-03.crt'
+    session.cert = cert_filename
     transport = Transport(session=session)
     client = zeep.Client(wsdl=wsdl_url, transport=transport)
     service = client.create_service(
@@ -31,12 +31,14 @@ def init_service(endpoint_url, service_agreement_uuid, user_system_uuid, user_uu
     return service, inv_context
 
 
-def main(endpoint_url, service_agreement_uuid, user_system_uuid, user_uuid, service_uuid):
+def main(endpoint_url, service_agreement_uuid, user_system_uuid, user_uuid, service_uuid, cert_filename):
 
-    service, inv_context = init_service(endpoint_url, service_agreement_uuid, user_system_uuid, user_uuid, service_uuid)
+    service, inv_context = init_service(endpoint_url, service_agreement_uuid, user_system_uuid, user_uuid, service_uuid, cert_filename)
 
-    #response = service.AddMunicipalityCodeSubscription(InvocationContext=inv_context, MunicipalityCode='0751')
-    response = service.RemoveMunicipalityCodeSubscription(InvocationContext=inv_context, MunicipalityCode='0751')
+
+    response = service.AddMunicipalityCodeSubscription(InvocationContext=inv_context, MunicipalityCode='0751')
+
+    #response = service.RemoveMunicipalityCodeSubscription(InvocationContext=inv_context, MunicipalityCode='0751')
 
     print response
 
