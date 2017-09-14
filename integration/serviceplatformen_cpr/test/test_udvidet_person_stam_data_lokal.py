e44ee24252f0d3e477691eea34c9d402c098359b
heiniovason <heini.ovason@gmail.com>
2017-09-14 15:13:04 +0200

    Some refactoring.

--- integration/serviceplatformen_cpr/test/test_udvidet_person_stam_data_lokal.py
+++ integration/serviceplatformen_cpr/test/test_udvidet_person_stam_data_lokal.py
@@ -1,7 +1,10 @@
 import unittest
 
-from services.udvidet_person_stam_data_lokal import get_citizen
-from services.udvidet_person_stam_data_lokal import validate_cprnr
+from services.udvidet_person_stam_data_lokal import (
+    get_citizen,
+    validate_cprnr,
+    call_cpr_person_lookup_request
+)
 
 __author__ = 'Heini Leander Ovason'
 
@@ -10,11 +13,23 @@ class TestUdvidetPersonStamDataLokal(unittest.TestCase):
 
     # ##### BEGIN TEST - get_citizen_data() #####
 
-    def test_get_citizen_jens_lyn_exactly_10_digits_OK(self):
-        self.assertEqual(
-            {'name': 'Jens Lyn'},
-            get_citizen('0123456789')
-        )
+    # def test_get_citizen_jens_lyn_exactly_10_digits_OK(self):
+    #
+    #     cprnr = '0123456789'
+    #
+    #     uuids = {
+    #         'service_agree': '11be3cb9-cd5e-45e8-801e-0fe9502c3a2c',
+    #         'user_sys': '11be3cb9-cd5e-45e8-801e-0fe9502c3a2c',
+    #         'user': '11be3cb9-cd5e-45e8-801e-0fe9502c3a2c',
+    #         'service': '11be3cb9-cd5e-45e8-801e-0fe9502c3a2c'
+    #     }
+    #
+    #     expected = ''
+    #
+    #     self.assertEqual(
+    #         'yeeha!',
+    #         get_citizen(service_uuids=uuids, cprnr=cprnr)
+    #     )
 
     # ##### END TEST - get_citizen_data() #####
 
@@ -40,6 +55,16 @@ class TestUdvidetPersonStamDataLokal(unittest.TestCase):
             validate_cprnr('012@456K8')
 
     # ##### END TEST - validate_cprnr() #####
+
+    # ##### BEGIN TEST - __call_cpr_person_lookup_request() #####
+
+    def test_call_cpr_person_lookup_request(self):
+        self.assertEqual(
+            'test',
+            call_cpr_person_lookup_request('0123456789')
+        )
+
+    # ##### END TEST - __call_cpr_person_lookup_request() #####
 
 
 if __name__ == '__main__':
