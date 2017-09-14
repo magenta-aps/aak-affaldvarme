008f02944ccd3932c4902cac1e7e44cd934d3504
heiniovason <heini.ovason@gmail.com>
2017-09-14 10:53:59 +0200

    Refactored test and test-subjects. Cosmetic and logical optimization.

--- integration/serviceplatformen_cpr/services/udvidet_person_stam_data_lokal.py
+++ integration/serviceplatformen_cpr/services/udvidet_person_stam_data_lokal.py
@@ -6,25 +6,42 @@ __author__ = "Heini Leander Ovason"
 
 # TODO:
 # facade function
-# validate cprnr
+# validate cprnr - OK
 # build SOAP envelope template.
 # call SP web service
 # parse SOAP xml response to dict
 
 
-def get_citizen_data(cprnr):
+def get_citizen(cprnr):
     """The function returnes a citizen dict from the
     'SF1520 - Udvidet person stamdata (lokal)' service.
-    It serves as a facade to simplify input validation and the SOAP interaction
-    with the service,and parsing the response to a dict.
-    return dict"""
+    It serves as a facade to simplify input validation, and interaction
+    with the SOAP service, and parsing the response to a dict.
+    :param cprnr: Danish cprnr
+    :type cpr: String of 10 digits / r'^\d{10}$'
+    :return: Dictionary representation citizen object.
+    :rtype: dict"""
 
-    if is_cprnr_valid(cprnr):
+    is_cprnr_valid = validate_cprnr(cprnr)
+
+    if is_cprnr_valid:
 
         return {'name': 'Jens Lyn'}
+
+
+def validate_cprnr(cprnr):
+
+    if cprnr is not None:
+
+        check = re.match(r'^\d{10}$', cprnr)
+
+        if check:
+
+            return True
+
+        else:
+
+            raise ValueError('"{}" is not a valid Danish cprnr.'.format(cprnr))
     else:
-        raise ValueError('{} is not a valid Danish cprnr.'.format(cprnr))
 
-
-def is_cprnr_valid(cprnr):
-    return re.match(r'^\d{10}$', cprnr)
+        raise TypeError('"{}" is not a valid type.'.format(cprnr))
--- integration/serviceplatformen_cpr/test/test_udvidet_person_stam_data_lokal.py
+++ integration/serviceplatformen_cpr/test/test_udvidet_person_stam_data_lokal.py
@@ -1,37 +1,45 @@
 import unittest
 
-from services.udvidet_person_stam_data_lokal import get_citizen_data
-from services.udvidet_person_stam_data_lokal import is_cprnr_valid
+from services.udvidet_person_stam_data_lokal import get_citizen
+from services.udvidet_person_stam_data_lokal import validate_cprnr
 
 __author__ = 'Heini Leander Ovason'
 
 
 class TestUdvidetPersonStamDataLokal(unittest.TestCase):
 
-    def test_get_citizen_data_jens_lyn(self):
+    # ##### BEGIN TEST - get_citizen_data() #####
+
+    def test_get_citizen_jens_lyn_exactly_10_digits_OK(self):
         self.assertEqual(
             {'name': 'Jens Lyn'},
-            get_citizen_data('0123456789')
+            get_citizen('0123456789')
         )
 
-    def test_get_citizen_data_incorrect_cprnr_ValueError(self):
-        # TODO: This implementation does not work!
-        self.assertRaises(
-            ValueError,
-            get_citizen_data('012345678')
-        )
+    # ##### END TEST - get_citizen_data() #####
 
-    def test_validate_cprnr_exactly_10_digits(self):
-        self.assertTrue(is_cprnr_valid('0123456789'))
+    # ##### BEGIN TEST - validate_cprnr() #####
 
-    def test_validate_cprnr_under_10_digits(self):
-        self.assertFalse(is_cprnr_valid('01234567890'))
+    def test_validate_cprnr_10_digits_OK(self):
+        self.assertTrue(validate_cprnr('0123456789'))
 
-    def test_validate_cprnr_above_10_digits(self):
-        self.assertFalse(is_cprnr_valid('012345678'))
+    def test_validate_cprnr_none_raise_exception(self):
+        with self.assertRaises(TypeError):
+            validate_cprnr(None)
 
-    def test_validate_cprnr_with_non_digits(self):
-        self.assertFalse(is_cprnr_valid('012@456K8'))
+    def test_validate_cprnr_9_digits_raise_exception(self):
+        with self.assertRaises(ValueError):
+            validate_cprnr('012345678')
+
+    def test_validate_cprnr_10_digits_raise_exception(self):
+        with self.assertRaises(ValueError):
+            validate_cprnr('01234567890')
+
+    def test_validate_cprnr_containing_non_digits_raise_exception(self):
+        with self.assertRaises(ValueError):
+            validate_cprnr('012@456K8')
+
+    # ##### END TEST - validate_cprnr() #####
 
 
 if __name__ == '__main__':
