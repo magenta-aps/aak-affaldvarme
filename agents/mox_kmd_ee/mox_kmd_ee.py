4233b0dbd7edb8d1203b60fe3c22f42fc617c97e
Carsten Agger <carstena@magenta.dk>
2017-09-20 13:07:02 +0200

    Agreements are added.

--- agents/mox_kmd_ee/mox_kmd_ee.py
+++ agents/mox_kmd_ee/mox_kmd_ee.py
@@ -4,8 +4,9 @@
 import pymssql
 
 from ee_sql import CUSTOMER_SQL
-from ee_oio import create_organisation, create_bruger
+from ee_oio import create_organisation, create_bruger, create_indsats
 from ee_oio import create_interessefaellesskab, create_organisationfunktion
+from ee_oio import lookup_bruger, lookup_organisation
 
 # Definition of strings used for Klassifikation URNs
 
@@ -36,6 +37,13 @@ def create_customer(id_number, key, name, phone="", email="",
             return result.json()['uuid']
 
 
+def lookup_customer(id_number):
+    if is_cpr(id_number):
+        return lookup_bruger(id_number)
+    elif is_cvr(id_number):
+        return lookup_organisation(id_number)
+
+
 def create_customer_role(customer_number, customer_uuid,
                          customer_relation_uuid, role):
     "Create an OrgFunktion from this info and return UUID"
@@ -60,13 +68,18 @@ def create_customer_relation(customer_number, customer_relation_name,
         return result.json()['uuid']
 
 
-def create_agreement(name, agreement_type, no_of_products, invoice_adress,
-                     address, start_date, end_date, property):
-    # TODO Create an Indsats from this info and return UUID
-    pass
+def create_agreement(name, agreement_type, no_of_products, invoice_address,
+                     address, start_date, end_date, location,
+                     customer_role_uuid):
+    "Create an Indsats from this info and return UUID"
+    result = create_indsats(name, agreement_type, no_of_products,
+                            invoice_address, address, start_date, end_date,
+                            location, customer_role_uuid)
+    if result:
+        return result.json()['uuid']
 
 
-def get_locations(customer_id):
+def get_products_for_location(location):
     # TODO Get locations for this customer ID from the Forbrugssted table
     return []
 
@@ -110,52 +123,70 @@ def connect(server, database, username, password):
 
 
 def import_all(connection):
-    cursor = connection.cursor(as_dict=True)
-    cursor.execute(CUSTOMER_SQL)
-    rows = cursor.fetchall()
+    # cursor = connection.cursor(as_dict=True)
+    # cursor.execute(CUSTOMER_SQL)
+    # rows = cursor.fetchall()
+    import csv
+    reader = csv.DictReader(open('Kunde.csv', 'r'))
+    rows = [r for r in reader]
     n = 0
     ligest_persons = 0
     print("Importing {} rows...".format(len(rows)))
     for row in rows:
-        # TODO: Insert customer in Lora if it doesn't exist already.
+        # Lookup customer in LoRa - insert if it doesn't exist already.
 
-        id_number = cpr_cvr(row['PersonnrSEnr'])
-        ligest_personnr = cpr_cvr(row['LigestPersonnr'])
-        customer_number = str(int(row['Kundenr']))
+        id_number = cpr_cvr(float(row['PersonnrSEnr']))
+        ligest_personnr = cpr_cvr(float(row['LigestPersonnr']))
+        customer_number = str(int(float(row['Kundenr'])))
 
-        customer_uuid = create_customer(
-            id_number,
-            customer_number,
-            row['KundeNavn'],
-            row['Telefonnr'],
-            row['EmailKunde'],
-            row['MobilTlf']
-        )
+        customer_uuid = lookup_customer(id_number)
 
-        if customer_uuid:
-            # New customer created
-            n += 1
-        else:
-            # No cake, no eating
-            continue
+        if not customer_uuid:
+            new_customer_uuid = create_customer(
+                id_number,
+                customer_number,
+                row['KundeNavn'],
+                row['Telefonnr'],
+                row['EmailKunde'],
+                row['MobilTlf']
+            )
 
-        # TODO: create customer relation
+            if new_customer_uuid:
+                # New customer created
+                customer_uuid = new_customer_uuid
+                n += 1
+            else:
+                # No customer created or found.
+                continue
+
+        # Create customer relation
+        # NOTE: In KMD EE, there's always one customer relation for each row in
+        # the Kunde table.
         cr_name = "<Varme + adresse fra SP>"
         cr_type = VARME  # Always for KMD EE
         cr_uuid = create_customer_relation(customer_number, cr_name, cr_type)
 
-        # TODO: This done, create customer roles & link customer and relation
+        assert(cr_uuid)
+
+        # This done, create customer roles & link customer and relation
         role_uuid = create_customer_role(customer_number, customer_uuid,
                                          cr_uuid, "Kunde")
         assert(role_uuid)
 
         # Now handle partner/roommate, ignore empty CPR numbers
         if len(ligest_personnr) > 1:
-            ligest_uuid = create_customer(
-                ligest_personnr, customer_number, row['KundeNavn']
-            )
+
+            ligest_uuid = lookup_customer(ligest_personnr)
+
+            if not ligest_uuid:
+                new_ligest_uuid = create_customer(
+                    ligest_personnr, customer_number, row['KundeNavn']
+                )
+                if new_ligest_uuid:
+                    ligest_uuid = new_ligest_uuid
+                    ligest_persons += 1
+
             if ligest_uuid:
-                ligest_persons += 1
                 create_customer_role(
                     customer_number, ligest_uuid, cr_uuid, "Ligestillingskunde"
                 )
@@ -168,28 +199,31 @@ def import_all(connection):
         start_date = row['Tilflytningsdato']
         end_date = row['Fraflytningsdato']
 
-        # For now, assume one agreement for each property (Forbrugssted)
+        # There is always one agreement for each location (Forbrugssted)
+        # AND only one location for each customer record.
 
         customer_id = row['KundeID']
 
-        for l in get_locations(customer_id):
-            products = get_products_for_location(l)
-            no_of_products = len(products)
+        location = row['ForbrugsstedID']  # TODO: Get BBR ID from Forbrugssted
+        products = get_products_for_location(location)
 
-            agreement_uuid = create_agreement(
-                name, agreement_type, no_of_products, invoice_adress, address,
-                start_date, end_date, l)
+        no_of_products = len(products)
 
-            for p in products:
-                name = "TODO: Fabrikat + Betegnel"
-                identification = "TODO: Instalnummer"
-                agreement = agreement_uuid
-                address = "TODO: Get from forbrugssted"
-                installation_type = VARME
-                meter_number = "TODO: Maalernr"
+        agreement_uuid = create_agreement(
+            name, agreement_type, no_of_products, invoice_address, address,
+            start_date, end_date, location, cr_uuid
+        )
+        assert(agreement_uuid)
+        for p in products:
+            name = "TODO: Fabrikat + Betegnel"
+            identification = "TODO: Instalnummer"
+            agreement = agreement_uuid
+            address = "TODO: Get from forbrugssted"
+            installation_type = VARME
+            meter_number = "TODO: Maalernr"
 
-                create_product(name, identification, agreement, address,
-                               installation_type, meter_number)
+            create_product(name, identification, agreement, address,
+                           installation_type, meter_number)
 
     print("Fandt {0} primære kunder og {1} ligestillingskunder.".format(
         n, ligest_persons)
@@ -197,9 +231,9 @@ def import_all(connection):
 
 
 if __name__ == '__main__':
-    from mssql_config import username, password, server, database
+    # from mssql_config import username, password, server, database
 
-    connection = connect(server, database, username, password)
+    connection = None  # connect(server, database, username, password)
     import_all(connection)
 
     # Test creation of virkning
