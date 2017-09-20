bbda225a773b7a9641ba773e1ae88f2334e87281
heiniovason <heini.ovason@gmail.com>
2017-09-20 14:36:41 +0200

    add+remove cprnr subscription pipeline implemented. Unit tests for testing Serviceplatform web service operation response implemented. Readme.rst updated with example on how to use cprnr_subscription_service()

--- integration/serviceplatformen_cpr/services/cpr_abonnement.py
+++ integration/serviceplatformen_cpr/services/cpr_abonnement.py
@@ -1,38 +1,111 @@
 # -- coding: utf-8 --
 import requests
 import settings
+import xmltodict
 
 from helpers.soap import construct_envelope_SF6002
 from helpers.validation import validate_cprnr
 from helpers.http_requester import http_post
 
 
-def add_cprnr_to_subscription(service_uuids, certificate, cprnr):
+def cprnr_subscription_service(service_uuids, certificate, cprnr, operation):
+    """The function serves as a facade to ease the interaction with
+    Serviceplatformen's 'SF6002 - CPR Abonnement' service.
+    The type of operation you want to perform depends on the type of operation
+    that is assigned to the operation parameter key when calling the function.
+    :param service_uuids: Serviceplatform invocation context uuids
+    :param certificate: Path to Serviceplatform certificate
+    :param cprnr: String of 10 digits -> r'^\d{10}$'
+    :param operation: 'add' or 'remove'
+    :type service_uuids: dict
+    :type certificate: str
+    :type cprnr: str
+    :type operation: str
+    :return: Response from the respective Serviceplatform web service operation
+    invoked.
+    :rtype: str"""
 
     is_cprnr_valid = validate_cprnr(cprnr)
 
     if is_cprnr_valid:
 
-        soap_envelope_template = settings.SP_SF6002_SOAP_ENVELOPE_TEMPLATE
+        result = None
 
-        # NOTE: This the name of the remote operation we want to invoke.
-        operation = 'AddPNRSubscription'
+        if operation == 'add':
 
-        soap_envelope = construct_envelope_SF6002(
-            template=soap_envelope_template,
-            service_uuids=service_uuids,
-            cprnr=cprnr,
-            operation=operation
-        )
+            result = invoke_operation(
+                service_uuids=service_uuids,
+                certificate=certificate,
+                cprnr=cprnr,
+                operation='AddPNRSubscription'
+            )
 
-        service_url = settings.SP_SERVICE_ENDPOINT_CPR_SUBSCRIPTION_1
+        elif operation == 'remove':
 
-        response = http_post(
-            endpoint=service_url,
-            soap_envelope=soap_envelope,
-            certificate=certificate)
+            result = invoke_operation(
+                service_uuids=service_uuids,
+                certificate=certificate,
+                cprnr=cprnr,
+                operation='RemovePNRSubscription'
+            )
 
-        if response.status_code == 200:
-            return response.text
         else:
-            return {'Error': 'Something went wrong'}
+
+            result = 'Invalid operation.'
+
+        return result
+
+
+def invoke_operation(service_uuids, certificate, cprnr, operation):
+    """The function is used to respectively invoke one of the two web service
+    operations, 'AddPNRSubscription' or 'RemovePNRSubscription', from
+    serviceplatformen's 'SF6002 - CPR Abonnement' service. It does this by
+    constructing a SOAP envelope based on the type of operation assigned to
+    the operation parameter key when calling the function.
+    :param service_uuids: Serviceplatform invocation context uuids
+    :param certificate: Path to Serviceplatform certificate
+    :param cprnr: String of 10 digits -> r'^\d{10}$'
+    :param operation: 'add' or 'remove'
+    :type service_uuids: dict
+    :type certificate: str
+    :type cprnr: str
+    :type operation: str
+    :return: Response from the respective Serviceplatform web service operation
+    invoked.
+    :rtype: str"""
+
+    soap_envelope_template = settings.SP_SF6002_SOAP_ENVELOPE_TEMPLATE
+
+    # NOTE: This is the name of the paramter for AddPNRSubscription
+    parameter_type = 'PNR'
+
+    soap_envelope = construct_envelope_SF6002(
+        template=soap_envelope_template,
+        service_uuids=service_uuids,
+        cprnr=cprnr,
+        operation=operation,
+        parameter_type=parameter_type
+    )
+
+    service_url = settings.SP_SERVICE_ENDPOINT_CPR_SUBSCRIPTION_1
+
+    response = http_post(
+        endpoint=service_url,
+        soap_envelope=soap_envelope,
+        certificate=certificate
+    )
+
+    if response.status_code == 200:
+
+        xml_to_dict = xmltodict.parse(response.text)
+
+        operation = 'ns2:{}Response'.format(operation)
+
+        result = xml_to_dict['soap:Envelope']['soap:Body'][
+            operation]['ns2:Result']
+
+        return result
+
+    else:
+
+        return str(response.status_code)
