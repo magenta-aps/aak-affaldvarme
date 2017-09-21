1de853947620b72f75b7d29179fa97343af150f4
heiniovason <heini.ovason@gmail.com>
2017-09-21 13:11:46 +0200

    Test cases defined - not implemented and tested

--- integration/serviceplatformen_cpr/test/test_validation.py
+++ integration/serviceplatformen_cpr/test/test_validation.py
@@ -1,13 +1,14 @@
 import unittest
 
 from helpers.validation import validate_cprnr
+from helpers.http_requester import http_post
 
 __author__ = 'Heini Leander Ovason'
 
 
 class TestValidation(unittest.TestCase):
 
-    # ##### BEGIN TEST - validate_cprnr() #####
+    # ##### BEGIN TEST CASES - validate_cprnr() #####
 
     def test_validate_cprnr_10_returns_false(self):
         self.assertTrue(validate_cprnr('0123456789'))
@@ -20,6 +21,20 @@ class TestValidation(unittest.TestCase):
 
     def test_validate_cprnr_11_digits_returns_false(self):
         self.assertFalse(validate_cprnr('01234567890'))
+
+    # ##### END TEST CASES - validate_cprnr() #####
+
+    # ##### BEGIN TEST CASES - http_post() #####
+
+    @unittest.expectedFailure
+    def test_http_post_parameters_are_not_None_return_response_object(self):
+        pass
+
+    @unittest.expectedFailure
+    def test_http_post_parameters_are_none_returns_none(self):
+        pass
+
+    # ##### END TEST CASES - http_post() #####
 
 
 if __name__ == '__main__':
