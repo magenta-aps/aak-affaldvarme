657349a78ad96a7f6554b08aac2bc80c0fa8e488
Carsten Agger <carstena@magenta.dk>
2017-09-25 14:39:27 +0200

    PEP-8 fixes.

--- aak_integration/serviceplatformen_cpr/cpr_helpers.py
+++ aak_integration/serviceplatformen_cpr/cpr_helpers.py
@@ -5,7 +5,7 @@ import serviceplatformen_cpr.settings as settings
 from serviceplatformen_cpr.services import get_citizen
 
 uuids = {
-    'service_agreement': settings.SP_SERVICE_AGREEMENT_UUID_SF1520_PROD, 
+    'service_agreement': settings.SP_SERVICE_AGREEMENT_UUID_SF1520_PROD,
     'user_system': settings.SP_USER_SYSTEM_UUID_PROD,
     'user': settings.SP_USER_UUID_PROD,
     'service': settings.SP_SERVICE_SF1520_PROD
@@ -29,4 +29,3 @@ if __name__ == '__main__':
     cprnr = '2511640019'
     result = get_cpr_data(cprnr)
     print(json.dumps(result))
-
