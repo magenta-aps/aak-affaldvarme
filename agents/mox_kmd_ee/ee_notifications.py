499c8b48fac7862ecc0edde089ee65700ee5269f
Carsten Agger <carstena@magenta.dk>
2017-10-16 14:36:57 +0200

    Last changes.

--- agents/mox_kmd_ee/ee_notifications.py
+++ agents/mox_kmd_ee/ee_notifications.py
@@ -2,7 +2,12 @@
 if __name__ == '__main__':
 
     import csv
+    import os
+    import glob
     import datetime
+    import difflib
+
+    from io import StringIO
 
     from mssql_config import username, password, server, database
     from ee_utils import connect
@@ -16,7 +21,7 @@ if __name__ == '__main__':
             writer.writerows(cursor)
 
     # Obtain CSV files for "now"
-
+    """
     connection = connect(server, database, username, password)
     cursor = connection.cursor()
     cursor.execute(CUSTOMER_AND_FORBRUGSSTED_SQL)
@@ -30,3 +35,36 @@ if __name__ == '__main__':
 
     csv_filename = 'tmp/trefinstallation_{0}.csv'.format(today)
     write_csv(csv_filename, cursor)
+    """
+   
+    customer_files = glob.glob('tmp/kunde_forbrugssted_*')
+    latest_customer_file = max(customer_files)
+
+    stored_customers = open(latest_customer_file, "r").read().split('\n')
+    
+    # Connect and get rows
+    connection = connect(server, database, username, password)
+    cursor = connection.cursor()
+    cursor.execute(CUSTOMER_AND_FORBRUGSSTED_SQL)
+    strio = StringIO()
+    writer = csv.writer(strio)
+    writer.writerow([i[0] for i in cursor.description])
+    writer.writerows(cursor)
+
+    customer_rows = strio.getvalue().split('\n')
+
+    print(stored_customers[0])
+    print(stored_customers[1])
+    print(customer_rows[0])
+    print(customer_rows[1])
+    
+    # Calculate diff
+    print('Calculating diff ...')
+    delta = difflib.ndiff(stored_customers, customer_rows)
+    print(len(stored_customers))
+    print(len(customer_rows))
+    delta = [r for r in delta if not r.startswith('  ')]
+    print('done!')
+
+    print(len(delta))
+
