cb467ff36ef1006c382e582927c07d02bcfda097
Steffen Park <dev@istherepie.com>
2017-10-19 15:34:52 +0200

    Implemented error logging to file (for objects that failed to process) Added log specific parameters to settings

--- agents/mox_error_handler/settings.py
+++ agents/mox_error_handler/settings.py
@@ -1,7 +1,11 @@
-# -*- coding: utf-8 -*-
+# Settings
 
 # Hostname of the MQ server
 MQ_HOST = "localhost"
 
 # Name of the queue
 MQ_QUEUE = "ava_mox_errors"
+
+# Log configuration
+DO_USE_DEBUG_LOG = False
+LOG_FILE = '/var/log/mox/ava_manual.log'
