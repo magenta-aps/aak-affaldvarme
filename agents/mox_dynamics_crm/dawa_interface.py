2640f4e7b8b314d76c16cab11c07b0b927a56d36
Steffen Park <dev@istherepie.com>
2017-12-12 10:47:02 +0100

    Added: import all addresses generator and adapter

--- agents/mox_dynamics_crm/dawa_interface.py
+++ agents/mox_dynamics_crm/dawa_interface.py
@@ -199,6 +199,113 @@ def get_access_address(uuid):
     return payload
 
 
+# IMPORT ALL ADDRESSES
+# TODO: Implement adapter for every function
+def adapter(data):
+
+    if not data:
+        return False
+
+    adresseid = data['id']
+    vejkode = data['vejkode']
+    vejnavn = data['vejnavn']
+    husnr = data['husnr']
+    etage = data['etage']
+    doer = data['dør']
+    postnr = data['postnr']
+    postnrnavn = data['postnrnavn']
+    kommunekode = data['kommunekode']
+    adgangsadresseid = data['adgangsadresseid']
+    kvhx = data['kvhx']
+
+    # Coordinates
+    koordinat_nord = data['wgs84koordinat_bredde']
+    koordinat_oest = data['wgs84koordinat_længde']
+
+    laengdegrad = data['etrs89koordinat_nord']
+    breddegrad = data['etrs89koordinat_øst']
+
+    # Todo:
+    # Country is currently hardcoded
+    # This may have to be dynamically set
+    land = 'Danmark'
+
+    # Split building id (numbers and letters)
+    husnr_nr = re.findall('\d+', husnr)[0]
+    husnr_bogstav = re.findall('\D+', husnr)
+
+    if husnr_bogstav:
+        husnr_bogstav = husnr_bogstav[0]
+    else:
+        husnr_bogstav = None
+
+    # Create address search string by combining all relevant values
+    search = '{} {}'.format(vejnavn, husnr)
+
+    if etage:
+        search += ', {}.'.format(etage)
+
+    if doer:
+        search += ' {}'.format(doer)
+
+    search += ', {} {}'.format(postnr, postnrnavn)
+
+    # AVA specific payload
+    payload = {
+        "_id": adresseid,
+        "_external": None,
+        "data": {
+            'ava_dawaadgangsadresseid': adgangsadresseid,
+            'ava_name': search,
+            'ava_gadenavn': vejnavn,
+            'ava_husnummer': husnr_nr,
+            'ava_bogstav': husnr_bogstav,
+            'ava_etage': etage,
+            'ava_doer': doer,
+            'ava_postnummer': postnr,
+            'ava_kommunenummer': kommunekode,
+            'ava_by': postnrnavn,
+            'ava_land': land,
+            'ava_kvhx': kvhx,
+            'ava_koordinat_nord': str(koordinat_nord),
+            'ava_koordinat_oest': str(koordinat_oest),
+            'ava_laengdegrad': str(laengdegrad),
+            'ava_breddegrad': str(breddegrad)
+        }
+    }
+    return payload
+
+
+def get_all(area_code):
+    """GET ADDRESS"""
+
+    resource = "adresser"
+
+    # Generate url
+    url = "{base}/{resource}".format(
+        base=BASE_URL,
+        resource=resource
+    )
+
+    # Params
+    params = {
+        "kommunekode": area_code,
+        "struktur": "flad"
+    }
+
+    # TODO: Verify SSL signature must dynamically be set
+    response = requests.get(
+        url=url,
+        params=params,
+        verify=DO_VERIFY_SSL_SIGNATURE
+    )
+
+    if response.status_code != 200:
+        return False
+
+    for address in response.json():
+        yield adapter(address)
+
 if __name__ == "__main__":
     address = get_access_address("0a3f5096-68f7-32b8-e044-0003ba298018")
     print(address)
