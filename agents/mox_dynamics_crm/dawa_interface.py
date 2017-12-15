24600b1ad1d16e1f1430dbaac0808693796887b8
Steffen Park <dev@istherepie.com>
2017-12-15 01:00:17 +0100

    Changed: get address function now returns the adapter

--- agents/mox_dynamics_crm/dawa_interface.py
+++ agents/mox_dynamics_crm/dawa_interface.py
@@ -39,97 +39,6 @@ def get_request(resource, uuid):
     return response.json()
 
 
-def get_address(uuid):
-
-    resource = "adresser"
-
-    data = get_request(
-        resource=resource,
-        uuid=uuid
-    )
-
-    if not data:
-        return
-
-    adresseid = data['id']
-    vejkode = data['vejkode']
-    vejnavn = data['vejnavn']
-    husnr = data['husnr']
-    etage = data['etage']
-    doer = data['dør']
-    postnr = data['postnr']
-    postnrnavn = data['postnrnavn']
-    kommunekode = data['kommunekode']
-    adgangsadresseid = data['adgangsadresseid']
-    kvhx = data['kvhx']
-
-    # According to the customer:
-    # koordinat_nord = laengdegrad
-    # Koordinat_oest = breddegrad
-    #
-    # example
-    # {
-    #     "ava_name": "Skoleparken 163, 8330 Beder",
-    #     "ava_koordinat_oest": "10.2106657",
-    #     "ava_koordinat_nord": "56.06605388",
-    #     "ava_laengdegrad": "6214092",
-    #     "ava_breddegrad": "575375.63"
-    # }
-    koordinat_nord = data['wgs84koordinat_bredde']
-    koordinat_oest = data['wgs84koordinat_længde']
-
-    laengdegrad = data['etrs89koordinat_nord']
-    breddegrad = data['etrs89koordinat_øst']
-
-    land = 'Danmark'
-
-    husnr_nr = re.findall('\d+', husnr)[0]
-    husnr_bogstav = re.findall('\D+', husnr)
-
-    if husnr_bogstav:
-        husnr_bogstav = husnr_bogstav[0]
-    else:
-        husnr_bogstav = None
-
-    # Create address search string by combining all relevant values
-    search = '{} {}'.format(vejnavn, husnr)
-
-    if etage:
-        search += ', {}.'.format(etage)
-
-    if doer:
-        search += ' {}'.format(doer)
-
-    search += ', {} {}'.format(postnr, postnrnavn)
-
-    # AVA specific payload
-    payload = {
-        # Hotfix:
-        # Adding redundant origin id
-        'origin_id': adresseid,
-
-        # Original payload
-        'ava_dawa_uuid': adresseid,
-        'ava_dawaadgangsadresseid': adgangsadresseid,
-        'ava_name': search,
-        'ava_gadenavn': vejnavn,
-        'ava_husnummer': husnr_nr,
-        'ava_bogstav': husnr_bogstav,
-        'ava_etage': etage,
-        'ava_doer': doer,
-        'ava_postnummer': postnr,
-        'ava_kommunenummer': kommunekode,
-        'ava_by': postnrnavn,
-        'ava_land': land,
-        'ava_kvhx': kvhx,
-        'ava_koordinat_nord': str(koordinat_nord),
-        'ava_koordinat_oest': str(koordinat_oest),
-        'ava_laengdegrad': str(laengdegrad),
-        'ava_breddegrad': str(breddegrad)
-    }
-    return payload
-
-
 def get_access_address(uuid):
 
     resource = "adgangsadresser"
@@ -253,7 +162,7 @@ def adapter(data):
     # AVA specific payload
     payload = {
         "_id": adresseid,
-        "_external": None,
+        "external_ref": None,
         "data": {
             'ava_dawaadgangsadresseid': adgangsadresseid,
             'ava_name': search,
@@ -275,6 +184,21 @@ def adapter(data):
         }
     }
     return payload
+
+
+def get_address(uuid):
+
+    resource = "adresser"
+
+    data = get_request(
+        resource=resource,
+        uuid=uuid
+    )
+
+    if not data:
+        return False
+
+    return adapter(data)
 
 
 def get_all(area_code):
@@ -316,6 +240,3 @@ def get_all(area_code):
 if __name__ == "__main__":
     address = get_access_address("0a3f5096-68f7-32b8-e044-0003ba298018")
     print(address)
-
-    addresses = get_all("0751")
-    print(addresses)
