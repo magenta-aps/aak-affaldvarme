82717e497343d0df5db773e6addead0631786a4e
Steffen Park <dev@istherepie.com>
2018-01-10 16:14:36 +0100

    Hotfix 21148: added function to delete duplicate contacts  - Fixed import requirements  - Fixed identifier for primary email update  - Added function to remove duplicate entries with incorrect uuid

--- agents/mox_dynamics_crm/update_client.py
+++ agents/mox_dynamics_crm/update_client.py
@@ -1,9 +1,12 @@
 # -*- coding: utf-8 -*-
 
+
 import logging
-from logger import start_logging
+import requests
 import crm_interface as crm
 import cache_interface as cache
+
+from logger import start_logging
 
 # Local settings
 from settings import LOG_FILE
@@ -50,7 +53,7 @@ def set_primary_email_for_contact():
 
         if secondary_email:
             # Gather information
-            identifier = contact["_id"]
+            identifier = contact["external_ref"]
 
             # Set primary email
             contact["data"]["emailaddress1"] = secondary_email
@@ -81,9 +84,76 @@ def set_primary_email_for_contact():
             )
 
 
+def remove_duplicate_contacts():
+    """
+    Support ticket (REDMINE): #21148
+    Remove duplicate entries created with Lora UUID
+    """
+
+    # Final list of contacts which must be updated
+    contacts_to_update = []
+
+    for contact in cache.find_all("contact"):
+
+        # if not contact["external_ref"]:
+        #     continue
+
+        if not "emailaddress1" in contact["data"]:
+            continue
+
+        primary_email = contact["data"]["emailaddress1"]
+        secondary_email = contact["data"]["ava_emailkmdee"]
+
+        if primary_email == secondary_email:
+            contacts_to_update.append(contact)
+
+    for contact in contacts_to_update:
+        # Get Lora UUID
+        lora_identifier = contact["_id"]
+        log.info("Handling: {}".format(lora_identifier))
+
+        # Create service url
+        resource = "contacts"
+
+        # Remove object from CRM
+        delete_contact = crm.delete_request(
+            resource=resource,
+            identifier=lora_identifier
+        )
+
+        if not delete_contact.status_code == 200:
+            log.error("Error deleting contact: lora_identifier")
+            log.error(delete_contact.text)
+
+            continue
+
+        try:
+            # Remove primary email address from cache
+            instructions_to_remove = {
+                "$unset": {
+                    "data.emailaddress1": ""
+                }
+            }
+
+            update_cache = cache.unset(
+                resource="contact",
+                identifier=lora_identifier,
+                instructions=instructions_to_remove
+            )
+            log.debug(update_cache)
+
+        except Exception as error:
+            log.error("Error updating cache")
+            log.error(error)
+
+
 if __name__ == "__main__":
+
     # Start logging
     start_logging(20, LOG_FILE)
+
+    # Hotfix:
+    remove_duplicate_contacts()
 
     # Run
     set_primary_email_for_contact()
