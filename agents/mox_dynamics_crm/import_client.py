14c53e3dae7d841d80b13252de576c4555f99c43
Steffen Park <steffen@magenta.dk>
2018-01-24 09:32:36 +0100

    Change: import client refactored and sphinx formatted docstrings added.

--- agents/mox_dynamics_crm/import_client.py
+++ agents/mox_dynamics_crm/import_client.py
@@ -5,29 +5,38 @@
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
-#
 
-import json
-import logging
 import requests
-
-# Local modules
-import cache_interface as cache
-import crm_interface as crm
 import oio_interface as oio
-import ava_adapter as adapter
 import dawa_interface as dawa
+import cache_interface as cache
 
-# Logging module
+from helper import get_config
 from logger import start_logging
 
-# Local settings
-from settings import ORGANISATION_UUID
-from settings import LOG_FILE
-from settings import DO_RUN_IN_TEST_MODE
-from settings import DO_DISABLE_SSL_WARNINGS
-from settings import AREA_CODE
+# Settings (For compatibility)
+config = get_config()
 
+# Parent organisation
+# Reference to which organisation an object belongs to
+# NOTE: Only some entities support this reference
+ORGANISATION_UUID = config["parent_organisation"] or None
+
+# Optionally the signature of the SSL certificate can be verified
+# This should be disabled when no commercial certificate is installed
+# (E.g. should be disabled or set to 'no' when using a self signed signature)
+# By default this is set to 'Yes'
+DO_VERIFY_SSL_SIGNATURE = config.getboolean("do_verify_ssl_signature", "yes")
+
+# General
+DO_DISABLE_SSL_WARNINGS = True
+DO_RUN_IN_TEST_MODE = True
+
+# Logging
+LOG_FILE = "/var/log/mox/mox_dynamics_crm.log"
+
+# DAR/DAWA settings
+AREA_CODE = "0751"
 
 # If the SSL signature is not valid requests will print errors
 # To circumvent this, warnings can be disabled for testing purposes
@@ -43,13 +52,15 @@ if DO_RUN_IN_TEST_MODE:
 
 
 # Set logging
-log = logging.getLogger(__name__)
+log = start_logging(20, LOG_FILE)
 
 
-### CACHE ###
 def import_all_addresses():
-    """ 
-    Missing docstring
+    """
+    Import all addresses from DAR (DAWA service)
+    and store them as documents in the cache layer
+
+    :return:    All operations are written to logs
     """
 
     # Begin
@@ -64,27 +75,38 @@ def import_all_addresses():
 
     if not addresses:
         log.warning(
-            "No addresses found in area code:  {0}".format(AREA_CODE)
+            "No addresses found in area code: {0}".format(AREA_CODE)
         )
         return False
 
-    # Store addresses
-    try:
-        cache.store_address(addresses)
-    except Exception as error:
-        log.error(error)
+    # Batch operation
+    size = 200
+
+    while len(addresses) > 0:
+        batch_of_addresses = addresses[:size]
+        addresses = addresses[size:]
+
+        try:
+            cache.store_address(batch_of_addresses)
+
+        except Exception as error:
+            log.error(batch_of_addresses)
+            log.error(error)
 
     # Finished procedure
     log.info("Finished processing all addresses")
 
 
 def import_to_cache(resource):
+    """
+    Import all database objects (by entity)
+
+    :param resource:    Name of the entity to import
+    :return:            Prints cache status object to terminal
+    """
 
     # Get all uuids
     list_of_uuids = oio.get_all(resource)
-
-    # Prepare cache payload
-    cache_payload = []
 
     # Workaround for consolidating bruger/organisation
     cache_resource = resource
@@ -97,35 +119,39 @@ def import_to_cache(resource):
     for entity in oio.batch_generator(resource, list_of_uuids):
         # # Append to the payload
         # cache_payload.append(entity)
-        store = cache.update_or_insert(cache_resource, entity)
+        store = cache.store(cache_resource, entity)
         print(store)
 
 
 def import_sanity_check():
+    """
+    Simple sanity check for addresses.
+    It iterates over all addresses stored in the cache layer,
+    and confirms that the address exists in the DAR database.
 
-    # Loop and do stuff
-    for address in cache.find_all("addresses"):
-        identifier = address.get("_id")
-        external = address.get("_external")
+    TODO: Create check
+    """
 
-        payload = {
-            "ava_dawa_uuid": identifier
-        }
+    # Retrieve all address from cache
+    addresses = cache.all("ava_adresses")
 
-        print("ava_adresses({0})".format(external))
-        print(json.dumps(payload))
+    for address in addresses:
+        identifier = address.get("id")
+
+        # Missing DAR check to confirm whether the address exists
+        # check_if_address_exists(identifier)
+        print(identifier)
 
 
-# RUN THE CLIENT
-if __name__ == "__main__":
+def run_import():
+    """
+    Run all import tasks
+    """
 
-    # Log to file
-    start_logging(20, LOG_FILE)
+    # Import addresses
+    import_all_addresses()
 
-    # Import all addresses
-    # import_all_addresses()
-
-    # Import to cache
+    # Import Lora objects
     import_to_cache("bruger")
     import_to_cache("organisation")
     import_to_cache("organisationfunktion")
@@ -138,3 +164,7 @@ if __name__ == "__main__":
 
     # Done
     print("Import procedure completed - Exiting")
+
+
+if __name__ == "__main__":
+    run_import()
