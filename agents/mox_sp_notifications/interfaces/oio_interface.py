5cf4e6159c928094999fe8b78f78c23d9a6063f9
Steffen Park <dev@istherepie.com>
2018-02-09 16:14:01 +0100

    20072: Code cleanup  - Added header  - Removed hardcoded configuration/settings  - Added docstrings

--- agents/mox_sp_notifications/interfaces/oio_interface.py
+++ agents/mox_sp_notifications/interfaces/oio_interface.py
@@ -1,13 +1,34 @@
-import logging
+# -- coding: utf-8 --
+#
+# Copyright (c) 2017, Magenta ApS
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+from logging import getLogger
 import requests
-from datetime import datetime
+from helper import get_config
 
-log = logging.getLogger(__name__)
 
-OIO_REST_URL = "https://minilab"
-ORGANISATION_UUID = "842092f6-7147-4349-bf8c-626757a28771"
-DO_VERIFY_SSL_SIGNATURE = False
+# Init logging
+log = getLogger(__name__)
 
+# Get config
+config = get_config("oio")
+
+# Settings
+OIO_REST_URL = config["oio_rest_url"]
+
+ORGANISATION_UUID = config["parent_organisation"]
+
+DO_VERIFY_SSL_SIGNATURE = config.getboolean(
+    "do_verify_ssl_signature", "yes"
+)
+
+# Resource switch
+# To retrieve the correct resource path
 switch = {
     "bruger": "organisation/bruger",
     "organisation": "organisation/organisation"
@@ -16,20 +37,21 @@ switch = {
 
 def get_all(resource):
     """
-    Wrapper function to retrieve all objects uuids,
+    Retrieve a list of all objects uuids,
     which belong to the parent organisation (see settings on top).
 
     :param resource:    Name of the resource path/entity
-    :return:            Returns a list of entity uuids
-                        (References only, not the actual database objects)
+
+    :return:            Returns a list of uuids
+                        (References only, not the actual objects)
     """
 
-
-    # Generate list of contacts (uuid)
+    # Info
     log.info(
         "Attempting to import: {0}".format(resource)
     )
 
+    # Get a list of all object uuids
     list_of_uuids = get_request(
         resource=resource,
         tilhoerer=ORGANISATION_UUID
@@ -52,24 +74,38 @@ def get_all(resource):
 
 def get_request(resource, **params):
     """
-    Parent function to perform the underlying GET request,
-    Returns object (or false if no objects are retrieved)
+    Generic function to perform a HTTP GET request.
 
-    :param resource:    Resource path, e.g. /service/path
-    :param params:      Query parameters
-    :return:
+    :param resource:    Resource path, e.g. /service/path,
+                        Needed in order to generate the service url
+
+    :param params:      (Optional) Query parameters
+
+    :return:            Returns a list of objects or None.
+
+                        NOTE:
+                        Currently OIO REST returns a list inside a list,
+
+                        As such, we return the first result,
+                        which is the actual list of objects.
+
+                        {
+                            "results": [
+                                [ <objects...> ]  <-- This is what we want
+                            ]
+                        }
     """
 
     # Use switch to determine resource path
-    resource = switch.get(resource)
+    resource_path = switch.get(resource)
 
     # Generate service url
     service_url = "{base_url}/{resource}".format(
         base_url=OIO_REST_URL,
-        resource=resource
+        resource=resource_path
     )
 
-    # GET REQUEST
+    # HTTP GET
     oio_response = requests.get(
         url=service_url,
         params=params,
@@ -80,62 +116,48 @@ def get_request(resource, **params):
     if oio_response.status_code != 200:
         return False
 
+    # Extract a list of data from the response
     results = oio_response.json()["results"]
 
     # Check if the list is empty
     if len(results) <= 0:
         return False
 
-    # Currently OIO REST returns a list inside a list,
-    # As such, we return the first result which is the actual list
-    # {
-    # "results": [
-    #     [ <objects...> ]  <-- This is what we want
-    #   ]
-    # }
     return results[0]
 
 
 def put_request(resource, identifier, payload):
     """
-    Parent function to perform the underlying GET request,
-    Returns object (or false if no objects are retrieved)
+    Generic function to perform a HTTP PUT request.
 
-    :param resource:    Resource path, e.g. /service/path
-    :param params:      Query parameters
-    :return:
+    In OIO Rest terms we use PUT requests update existing objects.
+    Alternatively a PUT request can also be used to import a new object
+    thereby preserving a pre-existing 'id' (Type: uuid)
+
+    :param resource_path:   Resource path, e.g. /service/path,
+                            Needed in order to generate the service url
+
+    :param params:          (Optional) Query parameters
+
+    :return:                Returns HTTP response object.
+
+                            For more information on the response object,
+                            please refer to the official documentation
+                            of the underlying requests library.
     """
 
     # Use switch to determine resource path
-    resource = switch.get(resource)
+    resource_path = switch.get(resource)
 
     # Generate service url
-    service_url = "{base_url}/{resource}/{uuid}".format(
+    service_url = "{base_url}/{path}/{uuid}".format(
         base_url=OIO_REST_URL,
-        resource=resource,
+        path=resource_path,
         uuid=identifier
     )
 
-    # PUT REQUEST
     return requests.put(
         url=service_url,
         json=payload,
         verify=DO_VERIFY_SSL_SIGNATURE
     )
-
-
-def update_bruger(identifier, payload):
-
-    response = put_request(
-        resource="bruger",
-        identifier=identifier,
-        payload=payload
-    )
-
-    if not response.status_code == 200:
-        print(response.text)
-
-    return response.json()
-
-
-
