b0ba3e3971fa5665621b91011fd32234cc38cb06
Steffen Park <dev@istherepie.com>
2018-02-11 23:38:54 +0100

    20072: Refactoring and docstrings added

--- agents/mox_sp_notifications/mox_sp_notifier.py
+++ agents/mox_sp_notifications/mox_sp_notifier.py
@@ -7,7 +7,6 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 #
 
-import json
 import requests
 from helper import start_logging
 from cpr_handler import cpr_handler
@@ -21,32 +20,27 @@ requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
 
 # Init log
-log = start_logging(10)
-
-
-# List of available tasks to run
-update_tasks = [
-    ("bruger", cpr_handler),
-    ("organisation", cvr_handler)
-]
-
-
-def all_tasks():
-    """
-    Wrapper
-    :return:
-    """
-
-    for resource, handler in update_tasks:
-        run_task(resource, handler)
+log_level = 20
+log = start_logging(log_level)
 
 
 def run_task(resource, handler):
     """
-    Import all database objects (by entity)
+    Main task runner.
+
+    Import all database objects (by entity),
+    iterate and run the given handler for each object.
+
+    The handler will return a list of updates
+    and these are applied to the original object.
+
+    Finally "send_update" is called to send an update request
+    to the OIO REST interface.
 
     :param resource:    Name of the entity to import
-    :return:            Prints cache status object to terminal
+
+    :return:            Nothing is returned,
+                        however events are written to log.
     """
 
     # Get all uuids
@@ -56,63 +50,160 @@ def run_task(resource, handler):
     log.debug(list_of_uuids)
 
     for uuid in list_of_uuids:
-        oio_object = oio.get_request(
+        # Retrieve
+        results = oio.get_request(
             resource=resource,
             uuid=uuid
         )
 
-        result = handler(oio_object)
-
-        if not result:
+        # Additional (paranoia) check
+        # List should only contain 1 result
+        if len(results) != 1:
             log.error(
-                "Unable to retrieve {resource}: {identifier}".format(
+                "OIO REST api has returned more than 1 result"
+            )
+
+            # Debug
+            log.debug(results)
+
+            continue
+
+        # Pick first result
+        result = results[0]
+
+        # Send result to the appropriate handler
+        list_of_updates = handler(result)
+
+        # Continue if there is nothing to update
+        if not list_of_updates:
+            log.info(
+                "No updates for '{resource}': {identifier}".format(
                     resource=resource,
                     identifier=uuid
                 )
             )
-            log.error(oio_object)
             continue
 
         # Info
-        log.info("Running updater")
+        log.info(
+            "The following items must be updated: {}".format(list_of_updates)
+        )
 
-        # Run updater
-        update_controller(resource, oio_object[0], result)
+        # Extract object from the 'regigisteringer's list
+        registrering = result['registreringer'][0]
+
+        # Iterate over the sections of the registrering object
+        # and apply updates for matched key/value pairs
+        for update in list_of_updates:
+
+            # Info
+            log.info("Applying update: {0}".format(update))
+
+            # Update registrering
+            apply_update(registrering, update)
+
+        # Send OIO update request
+        send_update(resource, uuid, registrering)
 
 
-def update_controller(resource, org_data, updates):
+def apply_update(registrering, update):
     """
-    Controller
+    TODO: Needs to be simplified
 
-    :param resource:    OIO resource, e.g. organisation
+    Dynamically traverse through the section and key of the object
+    in order to find matching key/value pairs which must be updated.
 
-    :param org_data:    OIO REST data object
+    If a matching key/value pair is found,
+    the values of the original keys are replaced with the updated values.
 
-    :param updates:     This is the dictionary returned by the handler
-                        Example:
+    Example:
 
-    :return:
+    "registrering":
+        {
+            "attributter": {  <-- "section"
+                "brugeregenskaber": [  <-- "key"
+                    {
+                        "ava_fornavn":  "Existing First Name"
+                    }
+                ]
+            }
+        }
+
+    "update":
+        {
+            "ava_fornavn":  "New First Name"
+        }
+
+    In this example the matching key/value pair is "ava_fornavn".
+    The value of the "existing" pair will be replaced with the "update" value.
+
+    :param registrering:    The full "registering" (OIO) object
+                            See example above.
+
+    :param update:          The update object containing key/value pairs
+                            which should be updated.
+                            See example above.
+
+    :return:                The function does not return anything,
+                            the "registering" object is modified/updated
+    """
+
+    # Map update dictionary
+    section = update["section"]
+    key = update["key"]
+    content = update["update"]
+
+    # Iterate over all items in the list
+    # Target the items that needs to be updated
+    for item in registrering[section][key]:
+
+        # Next we need to target the key/value pairs
+        # which need to be updated
+        for key in content.keys():
+
+            if key not in item.keys():
+                continue
+
+            # Replace the item values with updated values
+            item[key] = content[key]
+
+
+def send_update(resource, uuid, update):
+    """
+    Construct a HTTP request to update an existing object.
+
+    The expected response is HTTP 200,
+    if the expected response is not returned,
+    the event is written to the log along with the failed response.
+
+    The failed object is included in the log.
+    TODO:   the object contains sensitive data
+            and it may not be appropriate to include
+
+    For more detailed information on the request,
+    please see the underlying PUT request from the oio_interface module.
+
+    :param resource:    Name of the OIO entity
+
+    :param uuid:        Identifier of the exising object
+
+    :param update:      The modified/updated "registering" object
+                        (Full object with updated key/value pairs)
+
+    :return:            Returns boolean,
+                        additionally the HTTP response (200) is logged,
+                        e.g.
+                        Response: 200 (bruger: <identifier/uuid>)
     """
 
     # Info
-    log.info("Updating: {}".format(resource))
-
-    uuid = org_data["id"]
-    registreringer = org_data['registreringer'][0]
-
-    for update in updates:
-        # Debug
-        log.debug("Processing: {0}".format(update))
-        update_values(registreringer, update)
-
-    # Info
-    log.info("Updating Lora")
+    log.info("Sending update request")
 
     # Update PUT request
     response = oio.put_request(
         resource=resource,
         identifier=uuid,
-        payload=registreringer
+        payload=update
     )
 
     # Not updated (Not HTTP 200)
@@ -123,46 +214,36 @@ def update_controller(resource, org_data, updates):
             )
         )
 
+        # Log response
+        log.error(response.text)
+
         # Debug
         # Include failed object
-        log.error(registreringer)
+        log.error(update)
 
-    # Debug
-    log.debug(response.text)
+        return False
 
+    # Info
 
-def update_values(registreringer, update):
-    """
-    Dynamically build update objects
+    log.info(
+        "Response: {status} ({resource}: {identifier})".format(
+            status=response.status_code,
+            resource=resource,
+            identifier=uuid
+        )
+    )
 
-    :param registreringer:
-    :param update:
-    :return:
-    """
-    group = update["group"]
-    subgroup = update["subgroup"]
-    content = update["update"]
-
-    # TODO: Must be refactored into something elegant
-    # Iterate over all items in the subgroup list
-    # Target the items that needs to be updated
-    for item in registreringer[group][subgroup]:
-
-        # Content is the SP value (updated) value,
-        # which must replace the item value
-        for key in content.keys():
-
-            # We are not touch key/value pairs
-            # which does not exist in our update/content list
-            if not key in item.keys():
-                continue
-
-            # Replace item with updated content
-            item[key] = content[key]
+    return True
 
 
 if __name__ == "__main__":
 
-    # Run all tasks
-    # From the available tasks list
-    all_tasks()
+    # List of available tasks
+    available_tasks = [
+        ("bruger", cpr_handler),
+        ("organisation", cvr_handler)
+    ]
+
+    # Run all tasks from the list
+    for resource, handler in available_tasks:
+        run_task(resource, handler)
