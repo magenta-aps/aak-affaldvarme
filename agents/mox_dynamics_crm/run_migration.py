149ce8561fa3197f5ecfb9747f35bdf2b2cbd823
Steffen Park <dev@istherepie.com>
2018-04-11 15:07:19 +0200

    Added migration caller

---
+++ agents/mox_dynamics_crm/run_migration.py
@@ -0,0 +1,13 @@
+# -- coding: utf-8 --
+#
+# Copyright (c) 2017, Magenta ApS
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
+from migrations.migrate import run
+
+if __name__ == "__main__":
+    run()
