ef3c89809b0f150fc853567ffbe5af53e9a7f1b0
jgj-magenta-aps <jgj@magenta-aps.dk>
2018-06-26 15:08:32 +0200

    purge deactivated - does not delete but report

--- agents/mox_dynamics_crm/purge_client.py
+++ agents/mox_dynamics_crm/purge_client.py
@@ -21,13 +21,15 @@ log = getLogger(__name__)
 
 
 def delete_crm_entity(crm_table, lora_ref, external_ref):
+    """ deletes object in crm and then if it is subsequently not found there 
+        it is deleted in the cache_layer
+    """
 
-    if lora_ref and external_ref:
-        pass
-    else:
-        log.warn("ref error lora_ref:%s  external_ref:%s in %s", 
+    if not external_ref:
+        log.warn("ref error lora_ref:%s  external_ref:%s in %s delete in cache", 
             lora_ref, external_ref, crm_table
         )
+        cache.delete(crm_table, lora_ref)
         return
 
     response = crm.get_request(
@@ -92,6 +94,9 @@ def delete_crm_entity(crm_table, lora_ref, external_ref):
             )
 
 def get_obsolete_objects_dict(crm_table):
+    """ returns a dict with lora-id as key and object as value
+        for all objects that were not updated in the latest import
+    """
     return {o["id"]:o for o in cache.all_obsolete(crm_table)}
 
 def get_semi_safe_to_delete_objects_dict(
@@ -99,6 +104,17 @@ def get_semi_safe_to_delete_objects_dict(
     forbidden_keys, 
     objects 
 ):
+    """ copies all objects into semi_safe_to_delete
+        and then removes any object from semi_safe_to_delete that is directly
+        referenced by any incident in dynamics_crm
+        for contacts for example, it will disallow deleting if the objects 
+        external_ref is referred to by any incident in '_ava_aktoer_value'
+
+        The name semi_safe refers to the fact that this is only looking 
+        at direct references. 
+        It does not take into account that a contact is mentioned here
+        and then a customer_relation refers to that by use of an 'obind'
+    """
     semi_safe_to_delete=dict(objects)
     for k in forbidden_keys:
         for loraid, o in objects.items():
@@ -117,24 +133,25 @@ def find_incident_relations():
         "_customerid_value":[],
         "_ava_aktoer_value":[],
         "_ava_installation_value":[],
+
     }
 
-    response = crm.get_request(
-        resource="{table_name}".format(
-        table_name="incidents"
-        )
-    )
+    #response = crm.get_request(
+    #    resource="{table_name}".format(
+    #    table_name="incidents"
+    #    )
+    #)
 
-    if response.status_code != 200:
-        log.error("incidents not retrieved")
-        exit()
-    incidents = response.json()["value"]
-    refkeys=list(external_refs.keys())
-    for i in incidents:
-        for k in refkeys:
-            kval = i.get(k)
-            if kval: 
-                external_refs[k].append(kval)
+    #if response.status_code != 200:
+    #    log.error("incidents not retrieved")
+    #    exit()
+    #incidents = response.json()["value"]
+    #refkeys=list(external_refs.keys())
+    #for i in incidents:
+    #    for k in refkeys:
+    #        kval = i.get(k)
+    #        if kval: 
+    #            external_refs[k].append(kval)
 
     return { 
         k: set(v) 
@@ -163,56 +180,83 @@ def get_deleted_objects(objects, oio_resource):
 
     return deleted_objects
 
-def purge_objects(
+def purge_objects(purgable_objects, crm_table):
+    for loraid, o in purgable_objects.items():
+        delete_crm_entity(crm_table, loraid, o["external_ref"]) 
+
+def get_purgable_objects(
     crm_table, 
     forbidden_refs, 
     forbidden_keys,
 ):
     obsolete_objects = get_obsolete_objects_dict(crm_table)
-    semi_safe_to_delete = get_semi_safe_to_delete_objects_dict(
+    purgable_objects = get_semi_safe_to_delete_objects_dict(
         forbidden_refs,
         forbidden_keys,
         obsolete_objects
     )
-    safe_to_delete = semi_safe_to_delete
     # we dont need this now, but we had it earlier - to see if they are really deleted in lora
-    # safe_to_delete = get_deleted_objects(obsolete_objects, oio_resource)
-    log.warn("taking out %d of %s",len(safe_to_delete),crm_table)
+    # purgable_objects = get_deleted_objects(purgable_objects, oio_resource)
+    log.warn("purgable_objects %d of %s",len(purgable_objects),crm_table)
+    return purgable_objects
 
-    for loraid, o in safe_to_delete.items():
-        delete_crm_entity(crm_table, loraid, o["external_ref"]) 
 
 def purge_ava_kundeforhold(forbidden_refs):
     crm_table = "accounts"
     oio_resource=oio.resources["interessefaellesskab"]["resource"]
     forbidden_keys=["_ava_kundeforhold_value", "_customerid_value" ]
-    purge_objects(crm_table, forbidden_refs, forbidden_keys)
-        
+    purgable_objects = get_purgable_objects(
+        crm_table, 
+        forbidden_refs, 
+        forbidden_keys
+    )
+    purge_objects(purgable_objects, crm_table)
+
+
 def purge_ava_aftales (forbidden_refs):
     crm_table = "ava_aftales"
     oio_resource=oio.resources["indsats"]["resource"]
     forbidden_keys=[]
-    purge_objects(crm_table, forbidden_refs, forbidden_keys)
+    purgable_objects = get_purgable_objects(
+        crm_table, 
+        forbidden_refs, 
+        forbidden_keys
+    )
+    purge_objects(purgable_objects, crm_table)
+
 
 def purge_ava_installations(forbidden_refs):
     crm_table = "ava_installations"    
     oio_resource=oio.resources["klasse"]["resource"]
     forbidden_keys=["_ava_installation_value"] 
-    purge_objects(crm_table, forbidden_refs, forbidden_keys)
-
+    purgable_objects = get_purgable_objects(
+        crm_table, 
+        forbidden_refs, 
+        forbidden_keys
+    )
+    purge_objects(purgable_objects, crm_table)
 
 def purge_ava_contacts(forbidden_refs):
     crm_table = "contacts"    
     oio_resource=oio.resources["bruger"]["resource"]
     forbidden_keys=["_ava_aktoer_value"] 
-    purge_objects(crm_table, forbidden_refs, forbidden_keys)
-
+    purgable_objects = get_purgable_objects(
+        crm_table, 
+        forbidden_refs, 
+        forbidden_keys
+    )
+    purge_objects(purgable_objects, crm_table)
 
 def purge_ava_kunderolles(forbidden_refs):
     crm_table = "ava_kunderolles"    
     oio_resource=oio.resources["organisationfunktion"]["resource"]
     forbidden_keys=[]
-    purge_objects(crm_table, forbidden_refs, forbidden_keys)
+    purgable_objects = get_purgable_objects(
+        crm_table, 
+        forbidden_refs, 
+        forbidden_keys
+    )
+    purge_objects(purgable_objects, crm_table)
 
 
 def run_purge():
