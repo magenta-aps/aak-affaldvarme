66d0369517591399e886ffd64fe224afa3fdf625
jgj-magenta-aps <jgj@magenta-aps.dk>
2019-01-04 11:42:20 +0100

    udtraek done in mox_sp_notifications, abonnement done in own mox

---
+++ agents/mox_cpr_abonnement/__main__.py
@@ -0,0 +1,68 @@
+# -- coding: utf-8 --
+#
+# Copyright (c) 2019, Magenta ApS
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+import argparse
+import datetime
+import logging
+import os
+import json
+
+from mox_cpr_abonnement import (
+    lora_get_all_cpr_numbers,
+    cpr_add_subscription,
+    cpr_remove_subscription,
+    cpr_get_all_subscribed,
+)
+
+from settings import (
+    MOX_LOG_LEVEL,
+    MOX_JSON_CACHE,
+    SFTP_DOWNLOAD_PATH
+)
+
+# set warning-level for all loggers
+[
+    logging.getLogger(i).setLevel(logging.WARNING)
+    for i in logging.root.manager.loggerDict
+]
+
+logging.basicConfig(level=MOX_LOG_LEVEL)
+logger = logging.getLogger("mox_cpr_abonnement")
+logger.setLevel(logging.DEBUG)
+
+
+def update_cpr_subscriptions():
+    "add or remove subscriptions according to mora data"
+    logger.debug("update_cpr_subscriptions started")
+    must_subscribe = set(lora_get_all_cpr_numbers())
+    are_subscribed = set(cpr_get_all_subscribed())
+    add_set = must_subscribe - are_subscribed
+    remove_set = are_subscribed - must_subscribe
+    for pnr in remove_set:
+        cpr_remove_subscription(pnr)
+    for pnr in add_set:
+        cpr_add_subscription(pnr)
+    logger.debug("update_cpr_subscriptions ended")
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "--update-cpr-subscriptions",
+        help="find subscriptions for all cpr-numbers, update "
+        "subscriptions for the ones "
+        "that are missing in subscrition, remove the ones missing in mora",
+        action="store_true",
+        default=True,
+    )
+    args = parser.parse_args()
+
+    if args.update_cpr_subscriptions:
+        update_cpr_subscriptions()
---
+++ agents/mox_sp_notifications/change_lists.py
@@ -0,0 +1,48 @@
+import interfaces.oio_interface as oio
+import requests
+import datetime
+import cpr_udtraek
+from helper import get_config
+
+
+cpr_config = get_config("cpr_udtraek")
+oio_config = get_config("oio")
+
+
+def get_changed_cprs():
+    cprnos = []
+    yesterday = datetime.datetime.now() - datetime.timedelta(days=1)
+    citizen_changes_by_date = cpr_udtraek.delta(
+        yesterday.strftime("%y%m%d"), settings=cpr_config
+    )
+    [
+        cprnos.extend(x.keys()) 
+        for x in citizen_changes_by_date.values()
+    ]
+    return list(set(cprnos))
+
+
+def get_bruger_uuids_from_cprno(cprno):
+    return requests.get(
+        url="{oio_rest_url}/organisation/bruger".format(**oio_config),
+        params={"tilknyttedepersoner":"urn:{pnr}".format(pnr=cprno)},
+        verify=False
+    ).json()["results"][0]
+
+
+def get_changed_bruger_uuids():
+    changed_uuids = []
+    #return oio.get_all("bruger")  # current functionality
+    cprnos = get_changed_cprs()   # new functionality
+    for i in cprnos :
+        changed_uuids.extend(get_bruger_uuids_from_cprno(i))
+    return changed_uuids
+
+
+def get_changed_uuids(resource):
+    if resource == "bruger":
+        return get_changed_bruger_uuids()
+    else:
+        return oio.get_all(resource)
+
+
